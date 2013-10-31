3e6563b49d9ce3a3e7629cf5e9a0534528b543b0
Steven Woo <swoo@tackable.com>
2013-10-31 15:10:11 -0700

    exercises chapter 6

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render_to_response
 from rango.models import Category
 from rango.models import Page
 
+def encode_category_to_url(category):
+    category.url = category.name.replace(' ', '_')
 
 def index(request):
     # Obtain the context from the HTTP request.
@@ -11,13 +13,17 @@ def index(request):
 
     # Query for categories - add the list to our context dictionary.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages':page_list}
 
     # The following two lines are new.
     # We loop through each category returned, and create a URL attribute.
     # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
     for category in category_list:
-        category.url = category.name.replace(' ', '_')
+        encode_category_to_url(category)
+
 
     # Render the response and return to the client.
     return render_to_response('rango/index.html', context_dict, context)
