95c8a40e0f3537ba64f380c7715954fd77085e15
Steven Woo <swoo@tackable.com>
2013-11-04 09:52:09 -0800

    really finish 8.4

--- rango/forms.py
+++ rango/forms.py
@@ -25,3 +25,19 @@ class PageForm(forms.ModelForm):
         # Some fields may allow NULL values, so we may not want to include them...
         # Here, we are hiding the foreign key.
         fields = ('title', 'url','views')
+
+
+from rango.models import UserProfile
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
