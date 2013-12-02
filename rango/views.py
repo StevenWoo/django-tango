42cf96e1fe15619eeb17571c3a71377294a5ce6d
Steven Woo <swoo@tackable.com>
2013-12-02 12:24:18 -0800

    14.2.3 category view update

--- rango/views.py
+++ rango/views.py
@@ -102,30 +102,36 @@ def category(request, category_name_url):
     # We start by containing the name of the category passed by the user.
     context_dict = {'category_name': category_name}
 
-    try:
-        # Can we find a category with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
-        category = Category.objects.get(name=category_name)
+    cat_list = get_category_list()
+    context_dict['cat_list'] = cat_list
 
-        # Retrieve all of the associated pages.
+    try:
+        # Find the category with the given name.
+        # Raises an exception if the category doesn't exist.
+        # We also do a case insensitive match.
+        category = Category.objects.get(name__iexact=category_name)
+        context_dict['category'] = category
+        # Retrieve all the associated pages.
         # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
+        pages = Page.objects.filter(category=category).order_by('-views')
 
         # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
-        context_dict['category'] = category
-        context_dict['category_name_url'] = category_name_url
     except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
         # Don't do anything - the template displays the "no category" message for us.
         pass
 
     # Go render the response and return it to the client.
-    return render_to_response('rango/category.html', context_dict, context)
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            result_list = run_query(query)
+            context_dict['result_list'] = result_list
 
+    # Go render the response and return it to the client.
+    return render_to_response('rango/category.html', context_dict, context)
+    
 
 from rango.forms import CategoryForm
 @login_required
